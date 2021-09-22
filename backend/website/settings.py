c5c5f253042efd0c79b3fe697019b2d31b692e23
Ivan Katkov <ik52@nyu.edu>
2021-09-22 17:27:34 +0400

    Minor change of setting.py

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -161,8 +161,10 @@ if DEBUG:
         "http://127.0.0.1",
         "http://127.0.0.1:8080",
         "http://127.0.0.1:8081",
+        "http://localhost:8080",
+        "http://localhost:8081",
     ]
-    ALLOWED_HOSTS += ['127.0.0.1', 'testserver']
+    ALLOWED_HOSTS += ['127.0.0.1', 'localhost', 'testserver']
 
 
     # too slow... likely related to
