c07243c6ebf18ee13a1a976d1c520dbac6aad966
Ivan Katkov <ik52@nyu.edu>
2021-10-08 18:26:05 +0400

    Minor modification of e2e test

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -12,3 +12,31 @@ it('Check json format for /api/cubes', () => {
     .its('content-type')
     .should('include', 'application/json')
 })
+
+it('Check json format for /api/atlas_param', () => {
+  cy.request('/api/atlas_param?format=json')
+    .its('headers')
+    .its('content-type')
+    .should('include', 'application/json')
+})
+
+it('Check json format for /api/atlas_morphkin', () => {
+  cy.request('/api/atlas_morphkin?format=json')
+    .its('headers')
+    .its('content-type')
+    .should('include', 'application/json')
+})
+
+it('Check json format for /api/califa_object', () => {
+  cy.request('/api/califa_object?format=json')
+    .its('headers')
+    .its('content-type')
+    .should('include', 'application/json')
+})
+
+it('Check json format for /api/sami_cube_obs', () => {
+  cy.request('/api/sami_cube_obs?format=json')
+    .its('headers')
+    .its('content-type')
+    .should('include', 'application/json')
+})
