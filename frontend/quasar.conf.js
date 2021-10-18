995f4742cc9b3aee8eff11441cb33b27b7d8692a
Ivan Katkov <ik52@nyu.edu>
2021-10-18 17:09:59 +0400

    Fix routing and favicon links

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -53,7 +53,7 @@ module.exports = configure(function (ctx) {
 
       // rtl: true, // https://v2.quasar.dev/options/rtl-support
       // preloadChunks: true,
-      // showProgress: false,
+      showProgress: true,
       // gzip: true,
       // analyze: true,
 
@@ -74,6 +74,7 @@ module.exports = configure(function (ctx) {
 
       // for Vercel expectations
       // distDir: ctx.mode.spa ? 'public' : null,
+      // distDir: 'public',
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
@@ -153,27 +154,27 @@ module.exports = configure(function (ctx) {
         theme_color: '#027be3',
         icons: [
           {
-            src: 'icons/icon-128x128.png',
+            src: 'statics/icons/icon-128x128.png',
             sizes: '128x128',
             type: 'image/png',
           },
           {
-            src: 'icons/icon-192x192.png',
+            src: 'statics/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'icons/icon-256x256.png',
+            src: 'statics/icons/icon-256x256.png',
             sizes: '256x256',
             type: 'image/png',
           },
           {
-            src: 'icons/icon-384x384.png',
+            src: 'statics/icons/icon-384x384.png',
             sizes: '384x384',
             type: 'image/png',
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: 'statics/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
